d3bb451fd1e46b835786416622c7b31acf4de54c
NickLund <trojanscout@gmail.com>
2019-10-27 01:15:55 -0600

    Create cracker.py
    Made in VS-Code
    Windows sucks ass cuz crypt(3) doesn't exist and is too difficult to make work at 1AM
    Committing here so I can put it in a Kali VM so everything will be in one place and fucking work

---
+++ cracker.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import crypt
+    
+#dictionary attack function
+def crackAttempt(saltFull, trueHash):
+    with open("realhuman_phill.txt", "r") as crackerListFile:
+        for attemptWord in enumerate(crackerListFile):
+            #crypt takes password, then the salt with encoding type, then encrypts it
+            if (trueHash == crypt.crypt(attemptWord,saltFull)):
+                print("Hash: %s has password %s" % (trueHash, attemptWord))
+                break
+    
+def main():
+    #print crypt.crypt("puddle","$6$6b3y0laq")
+    # open file of pure hashes to compare with later
+    trueHashFile = open("compare.txt", "r")
+    
+    #counter for pure hash line
+    count = 1
+    encodingType = "$6$"
+    #open file of salts, pass single salt and hash into dictionary attack fuction
+    #With statement same as try: f=open('file') finally: f.close()
+    with open("salts.txt", "r") as saltListFile:
+        for saltStart in enumerate(saltListFile):
+            trueHash = trueHashFile.readline(count)
+            saltFull = encodingType + saltStart
+            crackAttempt(saltFull, trueHash)
+            count += 1
+    trueHashFile.close()
+
+if __name__ == "__main__":
+    main()
