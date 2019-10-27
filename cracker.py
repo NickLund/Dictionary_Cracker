22da2ee14aac272812b47ad3e367f976095f274d
NickLund <trojanscout@gmail.com>
2019-10-27 03:09:57 -0600

    Update cracker.py
    Fixed all errors
    restructured so readline would be strings
    restructured (made work the intended way) break method for crackAttempt function
    Added write to file

--- cracker.py
+++ cracker.py
@@ -6,29 +6,37 @@ import crypt
 def crackAttempt(saltFull, trueHash):
     with open("realhuman_phill.txt", "r") as crackerListFile:
         count = 1
-        for attemptWord in crackerListFile(count):
+        attemptWord = crackerListFile.readline()
+        while attemptWord:
             #crypt takes password, then the salt with encoding type, then encrypts it
-            count += 1
-            if (trueHash == crypt.crypt(attemptWord,saltFull)):
-                print("Hash: %s has password %s" % (trueHash, attemptWord))
+            hashed = crypt.crypt(attemptWord,saltFull)
+            if (trueHash == hashed):
+                print("Hash: %s has password %s. Too bad we moving so fast you can't read this huh?" % (trueHash, attemptWord))
+                fatCrackedPassList = open("FattyList.txt","wa+")
+                fatCrackedPassList.write("Hash: %s has password %s" % (trueHash, attemptWord))
+                fatCrackedPassList.close()
                 break
-    
+            print("Attempt number %s Password: %s is hashed as: %s" % (count, attemptWord, hashed))
+            count += 1
+            attemptWord = crackerListFile.readline()
+
 def main():
-    #print crypt.crypt("puddle","$6$6b3y0laq")
     # open file of pure hashes to compare with later
     trueHashFile = open("compare.txt", "r")
     
     #counter for pure hash line
     count = 1
-    encodingType = "$6$"
     #open file of salts, pass single salt and hash into dictionary attack fuction
     #With statement same as try: f=open('file') finally: f.close()
     with open("salts.txt", "r") as saltListFile:
-        for saltStart in saltListFile(count):
+        saltStart = saltListFile.readline()
+        while saltStart:
             trueHash = trueHashFile.readline(count)
-            saltFull = encodingType + saltStart
+            #saltFull = encodingType + saltStart
+            saltFull = saltStart
             crackAttempt(saltFull, trueHash)
             count += 1
+            saltStart = saltListFile.readline()
     trueHashFile.close()
 
 if __name__ == "__main__":
